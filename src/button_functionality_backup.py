015f6798dfbf2f6874b3c4185e64e7d7ec7f8c18
Nicknjano <nnjano254@gmail.com>
2023-12-23 23:26:23 +0300

    dashboard created

---
+++ src/button_functionality_backup.py
@@ -0,0 +1,9 @@
+"""This file contains a copy of the functions written to handle some of the buttons"""
+
+# self.retranslateUi(DashboardWindow)
+#         self.stackedWidget.setCurrentIndex(0)
+#         self.logoutButton.clicked.connect(DashboardWindow.close) # type: ignore
+#         self.homeButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0)) # type: ignore
+#         self.transactionsButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1)) # type: ignore
+#         self.manageAdminsButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3)) # type: ignore
+#         self.manageCustomersButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2)) # type: ignore
